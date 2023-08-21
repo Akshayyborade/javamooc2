b539a898eede0d184cca61e42f3df21ea9b9b4b5
Akshay borade <122223892+Akshayyborade@users.noreply.github.com>
2023-08-21 22:00:15 +0530

    new changes

---
+++ part08-Part08_13.VehicleRegistry/src/main/java/VehicleRegistry.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class VehicleRegistry {
+    private HashMap<LicensePlate, String> VehicleRegistry;
+
+    public VehicleRegistry() {
+        this.VehicleRegistry = new HashMap<>();
+    }
+
+    public boolean add(LicensePlate licensePlate, String owner) {
+        if (this.VehicleRegistry.containsKey(licensePlate)) {
+            return false;
+        } else {
+            this.VehicleRegistry.putIfAbsent(licensePlate, "");
+            // System.out.println("Added "+owner+" to the registry.");
+            return true;
+        }
+
+    }
+
+    public String get(LicensePlate licensePlate) {
+        try {
+            return this.VehicleRegistry.getOrDefault(licensePlate, "Not Found!");
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+
+    }
+
+    public boolean remove(LicensePlate licensePlate) {
+        if (!this.VehicleRegistry.containsKey(licensePlate)) {
+            return false;
+        } else {
+            for (Map.Entry entry : this.VehicleRegistry.entrySet()) {
+                if ((entry).getKey().equals((licensePlate))) {
+                    break;
+                }
+            }
+
+        }
+        return true;
+    }
+}
